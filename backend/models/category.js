0acdce64aeffb46b460ba462be15838644a9832a
chamrouensann <chamrouensann.geek@gmail.com>
2021-03-30 01:01:26 +0700

    Adding the models and routers

---
+++ backend/models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+});
+
+exports.Category = moongoose.model("Category", categorySchema);
